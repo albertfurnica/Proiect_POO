3e72c9206abca60e7b2907981ce143e9bd3f3238
Furnica Albert <albertfurnica2005@gmail.com>
2026-01-10 16:23:59 +0200

    Add files via upload

---
+++ ProiectPO/ProiectPO/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ProiectPO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int ok;
+
+        do
+        {
+            Console.WriteLine("Introduceti una dintre variante : ");
+            Console.WriteLine("1.Inchiriere auto");
+            Console.WriteLine("0.Iesire");
+            ok = int.Parse(Console.ReadLine());
+            switch (ok)
+            {
+                case 1:
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Introduceti o varianta corecta!");
+                    break;
+            }
+        } while (ok != 0);
+    }
+}
