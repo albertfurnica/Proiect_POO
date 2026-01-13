3b9045876fdd571ebdadf348b788df9b2e41cc62
Furnica Albert <albertfurnica2005@gmail.com>
2026-01-13 15:31:47 +0200

    am creat clasa rental

---
+++ ProiectPO/ProiectPO/Rental.cs
@@ -0,0 +1,15 @@
+﻿namespace ProiectPO;
+
+public class Rental
+{
+    public int Id { get; set; }
+    public Reservation Reservation { get; set; }
+    public double TotalCost { get; set; }
+
+    public Rental(int id, Reservation reservation, double totalCost)
+    {
+        Id = id;
+        Reservation = reservation;
+        TotalCost = totalCost;
+    }
+}
